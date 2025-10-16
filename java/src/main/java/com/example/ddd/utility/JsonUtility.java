09f81b934cc95f0effa56160c466df7961f11a16
chookrib <chookrib@gmail.com>
2025-10-16 22:58:22 +0800

    Changes

--- java/src/main/java/com/example/ddd/utility/JsonUtility.java
+++ java/src/main/java/com/example/ddd/utility/JsonUtility.java
@@ -1,8 +1,12 @@
 package com.example.ddd.utility;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.json.JsonReadFeature;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -19,7 +23,7 @@ import java.util.Map;
 public class JsonUtility {
 
     /**
-     * 读取 JsonNode
+     * 读取 JSON 字符串为 JsonNode
      */
     public static JsonNode readTree(String json) {
         try {
@@ -52,6 +56,39 @@ public class JsonUtility {
     //    //    throw new RuntimeException("对象转为Map异常", e);
     //    //}
     //}
+
+    /**
+     * 读取 JSON 字符串为 JsonNode，允许未转义的控制字符
+     */
+    public static JsonNode readTreeAllowUnescapedControlChars(String content) {
+        ObjectMapper objectMapper = JsonMapper.builder()
+                .enable(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS)
+                .build();
+        try {
+            return objectMapper.readTree(content);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 读取 JSON 字符串为指定类型对象
+     */
+    public static <T> T readValue(String content, TypeReference<T> valueTypeRef) {
+        try {
+            return new ObjectMapper().readValue(content, valueTypeRef);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 创建 ObjectNode
+     */
+    public static ObjectNode createObjectNode() {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.createObjectNode();
+    }
 
     // =================================================================================================================
     // 以下为用于JSON转换测试的数据定义
