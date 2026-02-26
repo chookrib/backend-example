76a0ccf3ae0b05277917ee9dc0eed2a1eb50f0c4
chookrib <chookrib@gmail.com>
2026-02-26 19:02:50 +0800

    Changes

--- backend-example-python/src/application/user_dto.py
+++ backend-example-python/src/application/user_dto.py
@@ -15,14 +15,14 @@ class UserDto(BaseModel):
     created_at: datetime
     updated_at: datetime
 
-    def to_json(self):
-        return {
-            "id": self.id,
-            "username": self.username,
-            "nickname": self.nickname,
-            "mobile": self.mobile,
-            "isAdmin": self.is_admin,
-            # "createdAt": self.created_at.isoformat(),
-            "createdAt": self.created_at,
-            "updatedAt": self.updated_at
-        }
+    # def to_json(self):
+    #     return {
+    #         "id": self.id,
+    #         "username": self.username,
+    #         "nickname": self.nickname,
+    #         "mobile": self.mobile,
+    #         "isAdmin": self.is_admin,
+    #         # "createdAt": self.created_at.isoformat(),
+    #         "createdAt": self.created_at,
+    #         "updatedAt": self.updated_at
+    #     }
